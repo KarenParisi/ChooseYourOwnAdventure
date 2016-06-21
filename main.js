3679178f2843e430a8739f48ecdc25ff7c56c18b
Karen Parisi <karen@Karens-Air.PK5001Z>
2016-06-21 06:11:13 -0700

    completed

---
+++ main.js
@@ -0,0 +1,43 @@
+confirm ("Do you want to play?")
+console.log ("confirm")
+
+var eligibility = prompt("Do you like cats?");
+if (eligibility == "yes")
+{
+	alert("Great! Please continue.");
+}
+else 
+{
+	alert("Sorry, you cannot play.");
+}
+	alert("You are a sweet house cat looking for some fun, what to do? You notice one of the humans sitting at the table.");
+
+var userAnswer = prompt("Should you approach the human with your loudest high pitch loud meow to get their attention?");
+if (userAnswer == "yes") {
+	alert("Good choice, the human comes running over with toys! The best, playing catch with my favorite human!");
+}
+else {
+	alert("Good idea, they are too busy working on the computer");
+	alert("Will just settle for long nap instead.");
+}	
+alert("Now you are noticing the new sofa.");
+var userAnswer = prompt("Should you give it a scratch?");
+if (userAnswer == "yes") {
+	alert("Oh no, you activate dthe puffer, so scary!");
+	alert("Will just settle for long nap instead.");
+}	
+else {
+	alert("Better not to, too many humans around to witness");
+	alert("I'll camp out at window and taunt the chpmonk outside, much more fun!");
+}
+alert("Just noticed some activity in outside.");
+var userAnswer = prompt ("Should I go camp out at the window?");
+if (userAnswer == "yes") {
+	alert("Good timing, the chipmunk is on the patio.");
+	alert("So much fun taunting the chipmonk!")
+}
+else {
+	alert("Will just settle for long nap instead");
+}
+
+
